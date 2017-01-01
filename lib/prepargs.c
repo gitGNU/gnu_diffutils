90106d33ab3004872f8cfe46779683be48b7bd12
Jim Meyering <meyering@fb.com>
2017-01-01 03:22:44 -0800

    maint: update gnulib and copyright dates for 2017
    * gnulib: Update to latest.
    * all files: Run "make update-copyright".

--- lib/prepargs.c
+++ lib/prepargs.c
@@ -1,6 +1,6 @@
 /* Parse arguments from a string and prepend them to an argv.
 
-   Copyright (C) 1999-2002, 2006, 2009-2013, 2015-2016 Free Software
+   Copyright (C) 1999-2002, 2006, 2009-2013, 2015-2017 Free Software
    Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
--- src/normal.c
+++ src/normal.c
@@ -1,6 +1,6 @@
 /* Normal-format output routines for GNU DIFF.
 
-   Copyright (C) 1988-1989, 1993, 1995, 1998, 2001, 2006, 2009-2013, 2015-2016
+   Copyright (C) 1988-1989, 1993, 1995, 1998, 2001, 2006, 2009-2013, 2015-2017
    Free Software Foundation, Inc.
 
    This file is part of GNU DIFF.
